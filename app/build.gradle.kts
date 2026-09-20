fe3597a6ea6a7d77d8a5e6015c573bf62bec2108
d01226091-wq <d01226091@gmail.com>
2026-09-20 11:00:25 +0300

    Configure app

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.bedrockplayeranalyzer"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.bedrockplayeranalyzer"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.1"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
